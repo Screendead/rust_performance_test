823c1a936595a8357c87f28c4c563bfb60298f69
Jack <jack@lusher.digital>
2023-03-18 20:36:30 +0000

    Add tests

--- src/main.rs
+++ src/main.rs
@@ -2,7 +2,7 @@ use std::thread;
 use std::time::Instant;
 
 fn main() {
-    let max_threads = num_cpus::get();
+    let max_threads = max_threads();
     let mut threads = vec![];
 
     let start_time = Instant::now();
@@ -10,11 +10,7 @@ fn main() {
     for i in 1..=max_threads {
         let thread_start_time = Instant::now();
         let thread = thread::spawn(move || {
-            let mut result: i128 = 0;
-
-            for j in 1..=10_000_000_000 {
-                result += j;
-            }
+            let result: i64 = thread_result(100_000_000);
 
             let thread_elapsed = thread_start_time.elapsed();
             println!(
@@ -34,4 +30,35 @@ fn main() {
 
     let total_elapsed = start_time.elapsed();
     println!("Total elapsed time: {} ms", total_elapsed.as_millis());
+}
+
+fn max_threads() -> usize {
+    num_cpus::get()
+}
+
+fn thread_result(max: usize) -> i64 {
+    let mut result: i64 = 0;
+
+    for i in 1..=max {
+        result += i as i64;
+    }
+
+    result
+}
+
+#[test]
+fn test_max_threads() {
+    let expected = num_cpus::get();
+    let actual = max_threads();
+    assert_eq!(expected, actual);
+}
+
+#[test]
+fn test_thread_result() {
+    let expected = 5000050000;
+    let start_time = Instant::now();
+    let actual = thread_result(100_000);
+    let elapsed = start_time.elapsed().as_millis();
+    assert_eq!(expected, actual);
+    println!("Thread result test passed in {} ms", elapsed);
 }
